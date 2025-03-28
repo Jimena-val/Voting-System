709657a2012366b69c3f0b9e3f601c9d6ed5ad4b
Jimena-val <andrea.valencia@iest.edu.mx>
2025-03-28 16:49:06 -0600

    Update server.js
    Cambio de IP estática a dinámica para Railway

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-// server.js
-
 const WebSocket = require('ws');
-//CAMBIAR IP POR LA IP DE MI PC
-const wss = new WebSocket.Server({ port: 9090, host: '192.168.0.10' });
+
+// Usamos el puerto asignado dinámicamente por Railway
+const port = process.env.PORT || 9090;  // Usa el puerto proporcionado por Railway o un puerto local si no está definido
+const wss = new WebSocket.Server({ port: port });
 
 let count = 0;
 
 wss.on('connection', function connection(ws) {
-    // Envía el contador actual a la nueva conexión
     ws.send(JSON.stringify({ type: 'count', data: count }));
 
-    // Maneja los mensajes recibidos desde el cliente
     ws.on('message', function incoming(message) {
         const data = JSON.parse(message);
         if (data.type === 'increment') {
-            // Incrementa el contador externo según el valor enviado desde el cliente
             count += data.value;
             wss.clients.forEach(function each(client) {
                 if (client.readyState === WebSocket.OPEN) {
                     client.send(JSON.stringify({ type: 'count', data: count }));
                 }
             });
-        } else if (data.type === 'reset') { // Maneja el reinicio del contador
+        } else if (data.type === 'reset') {
             count = 0;
             wss.clients.forEach(function each(client) {
                 if (client.readyState === WebSocket.OPEN) {
@@ -31,3 +28,5 @@ wss.on('connection', function connection(ws) {
         }
     });
 });
+
+console.log(`Server running on port ${port}`);
